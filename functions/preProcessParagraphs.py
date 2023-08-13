07e1daf2f2bf8e1ef94a1ddbf8c4f1a01cdaf58a
kemalsahin2001 <kemals@rocketmail.com>
2023-08-13 14:42:56 +0300

    button function added

---
+++ functions/preProcessParagraphs.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import string
+
+def process_text(paragraph):
+    # Tokenize the paragraph and keep track of start index of each word
+    words = word_tokenize(paragraph)
+    
+    # Calculate the starting position of each word in the original paragraph
+    word_positions = {word: paragraph.find(word) for word in words}
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word.lower() not in stop_words]
+    
+    # Identify and remove proper nouns
+    pos_tags = nltk.pos_tag(words)
+    words = [word for word, pos in pos_tags if pos != 'NNP' and pos != 'NNPS']
+    
+    # Remove punctuation
+    words = [word for word in words if word not in string.punctuation]
+    
+    # Return the processed words and their positions
+    return [(word, word_positions[word]) for word in words]
