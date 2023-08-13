f1e584709cc22926851fb2b0ba8bd99931cdc99c
kemalsahin2001 <kemals@rocketmail.com>
2023-08-13 23:19:44 +0300

    bug fix

--- functions/preProcessParagraphs.py
+++ functions/preProcessParagraphs.py
@@ -14,25 +14,26 @@ for i in range(0xFFFF):  # You can increase this range if necessary
 
 def process_text(paragraph):
     # Tokenize the paragraph without splitting contractions
-    words = re.findall(r"\b\w+\b", paragraph)
+    pattern = r"\b\w+(?:['’]\w+)?\b"
+    words = re.findall(pattern, paragraph)
     
     # Calculate the starting and ending position of each word in the original paragraph
     word_positions = []
     last_index = 0
     for word in words:
         start_position = paragraph.find(word, last_index)
-        word_positions.append((word, start_position))
+        word_positions.append((word, start_position, "button"))
         last_index = start_position + len(word)
     
     # Remove stopwords
     stop_words = set(stopwords.words('english'))
-    word_positions = [(word, start) for word, start in word_positions if word.lower() not in stop_words]
+    word_positions = [(word, start, "stop-word") if word.lower() in stop_words else (word, start, lbl) for word, start, lbl in word_positions]
     
     # Identify and remove proper nouns
-    pos_tags = nltk.pos_tag([word for word, _ in word_positions])
-    word_positions = [(word, start) for (word, start), (_, tag) in zip(word_positions, pos_tags) if tag not in ['NNP', 'NNPS']]
+    pos_tags = nltk.pos_tag([word for word, _, _ in word_positions])
+    word_positions = [(word, start, "proper-noun") if tag in ['NNP', 'NNPS'] else (word, start, label) for (word, start, label), (_, tag) in zip(word_positions, pos_tags)]
     
     # Remove punctuation
-    word_positions = [(word, start) for word, start in word_positions if word not in punctuations]
+    word_positions = [(word, start,lbl) for word, start,lbl in word_positions if word not in punctuations]
 
     return word_positions
