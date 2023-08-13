d9506a58fcab9ab4699c66f157cb97f670435b15
kemalsahin2001 <kemals@rocketmail.com>
2023-08-13 22:01:23 +0300

    bug fix

--- functions/preProcessParagraphs.py
+++ functions/preProcessParagraphs.py
@@ -16,23 +16,23 @@ def process_text(paragraph):
     # Tokenize the paragraph without splitting contractions
     words = re.findall(r"\b\w+\b", paragraph)
     
-    # Calculate the starting position of each word in the original paragraph
-    word_positions = {}
+    # Calculate the starting and ending position of each word in the original paragraph
+    word_positions = []
     last_index = 0
     for word in words:
-        word_positions[word] = paragraph.find(word, last_index)
-        last_index = word_positions[word] + len(word)
+        start_position = paragraph.find(word, last_index)
+        word_positions.append((word, start_position))
+        last_index = start_position + len(word)
     
     # Remove stopwords
     stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word.lower() not in stop_words]
+    word_positions = [(word, start) for word, start in word_positions if word.lower() not in stop_words]
     
     # Identify and remove proper nouns
-    pos_tags = nltk.pos_tag(words)
-    words = [word for word, pos in pos_tags if pos != 'NNP' and pos != 'NNPS']
+    pos_tags = nltk.pos_tag([word for word, _ in word_positions])
+    word_positions = [(word, start) for (word, start), (_, tag) in zip(word_positions, pos_tags) if tag not in ['NNP', 'NNPS']]
     
     # Remove punctuation
-    words = [word for word in words if word not in punctuations]
-    
-    # Return the processed words and their positions
-    return [(word, word_positions[word]) for word in words]
+    word_positions = [(word, start) for word, start in word_positions if word not in punctuations]
+
+    return word_positions
